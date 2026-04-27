0337a86fc9c0097d1954574beb7f3c9c5fe825ab
Shun Sato <shun.sato.mist@gmail.com>
2026-04-27 20:41:38 +0900

    Create comparison.c

---
+++ 4/comparison.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+    int a = 1, b = 2;
+    // 真なら1，偽なら0が出力される
+    printf("%d\n",a < b);
+    printf("%d\n",a <= b);
+    printf("%d\n",a > b);
+    printf("%d\n",a >= b);
+}
