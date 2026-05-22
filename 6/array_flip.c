227d79a1f7e960422fe43ec02fcfd354ce356ec8
Shun Sato <shun.sato.mist@gmail.com>
2026-05-22 18:11:44 +0900

    Update array_flip.c

--- 6/array_flip.c
+++ 6/array_flip.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#define NUM 7 // 配列の大きさを管理
+#define SIZE 7 // 配列の大きさを管理
 int main(void){
-    int a[NUM]; 
+    int a[SIZE]; 
     // 入力
-    for (int i=0;i<NUM;i++) scanf("%d",&a[i]);
+    for (int i=0;i<SIZE;i++) scanf("%d",&a[i]);
     // 配列の反転
-    for (int i=0;i<NUM/2;i++) {
+    for (int i=0;i<SIZE/2;i++) {
         int tmp = a[i];
-        a[i] = a[NUM-i-1];
-        a[NUM-i-1] = tmp;
+        a[i] = a[SIZE-i-1];
+        a[SIZE-i-1] = tmp;
     }
     // 表示
-    for (int i=0;i<NUM;i++) printf("%d\n",a[i]);
+    for (int i=0;i<SIZE;i++) printf("%d\n",a[i]);
 }
