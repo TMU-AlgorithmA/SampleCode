b08dbd98171948b4b44796b7d0ed0d661ffbcf72
Shun Sato <shun.sato.mist@gmail.com>
2026-06-30 15:37:01 +0900

    Update copy_string.c

--- 11/copy_string.c
+++ 11/copy_string.c
@@ -4,8 +4,10 @@
 
 char *copy_string(char *in){
     char *out = malloc(sizeof(char)*(strlen(in)+1));
-    if (out == NULL) // メモリ確保失敗の場合の処理
+    if (out == NULL) {// メモリ確保失敗の場合の処理
         fprintf(stderr, "メモリ確保失敗．\n"); // エラーメッセージ
+        return NULL;
+    }
     out[0] = in[0];
     for (int i=1; in[i-1]; i++) // 文字列のコピー (ヌル文字まで)
         out[i] = in[i];
